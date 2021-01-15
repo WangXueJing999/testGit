dab732f703d363e406dc90faffdd0ed8936369bf
WangXueJing999 <1919671189@qq.com>
2021-01-15 16:18:48 +0800

    解决冲突

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -6,11 +6,8 @@ public class HelloGit {
 		System.out.println("Hello Git!");
 		System.out.println("更新1");
 		System.out.println("创建分支");
-<<<<<<< HEAD
 		System.out.println("主干添加");
-=======
 		System.out.println("分支添加");
->>>>>>> refs/heads/dev
 
 	}
 
